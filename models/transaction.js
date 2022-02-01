fdcbb72325fc3bafa9a5ad874c5e8e17a71b30ad
Awad Sharif <awadsharif9@gmail.com>
2022-01-31 19:31:14 -0800

    added data to models

---
+++ models/transaction.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//         type: 'resistance',
+// name: 'Bicep Curl',
+// duration: 20,
+// weight: 100,
+// reps: 10,
+// sets: 4,
+
+const transactionSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a workout name"
+  },
+  duration: {
+    type: Number,
+    required: "Enter a duration amount"
+  },
+  weight: {
+    type: Number,
+    required: "Enter a weight amount"
+  },
+  reps: {
+    type: Number,
+    required: "Enter the amount of reps"
+  },
+  sets: {
+    type: Number,
+    required: "Enter the amount of reps"
+  },
+  distance: {
+      type: Number,
+      required: "Enter the distance in miles"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
